53aa0c05bafffe1e72cd0e8d9c2ba0b8e423fd4d
emre can yılmaz <ecylmz@bil.omu.edu.tr>
2011-09-04 22:17:48 +0300

    domain'i aktif etmek için sembolik link eklendi.

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,6 +1,7 @@
 class nginx ($domain = undef, $enable = true, $ensure = running) {
 
-	$conf_file = "/etc/nginx/sites-available/${domain}"
+	$conf_file        = "/etc/nginx/sites-available/${domain}"
+	$conf_file_enabled = "/etc/nginx/sites-enabled/${domain}"
 
 	package { "nginx":
 		ensure => latest
@@ -10,8 +11,13 @@ class nginx ($domain = undef, $enable = true, $ensure = running) {
 		source => "puppet:///nginx/${domain}"
 	}
 
+	file { "${conf_file_enabled}":
+		ensure => link,
+		target => "${conf_file}"
+	}
+
 	service { "nginx":
 		ensure    => $ensure,
-		subscribe => [Package["nginx"], File["${conf_file}"]]
+		subscribe => [Package["nginx"], File["${conf_file_enable}"]]
 	}
 }
